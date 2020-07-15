be524af81fe7cac7a8a31ec8f01b51540a3e99ac
Chris Alvarez <alvarezfchris@gmail.com>
2020-07-15 12:57:09 -0500

    pending tests

--- src/test/java/com/example/brightcove/controller/PokemonControllerTest.java
+++ src/test/java/com/example/brightcove/controller/PokemonControllerTest.java
@@ -1,23 +1,32 @@
 package com.example.brightcove.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import com.example.brightcove.entity.PokemonRelation;
+import com.example.brightcove.service.PokemonService;
 
 @DisplayName("Tests for PokemonController")
 public class PokemonControllerTest {
 
 	@Autowired
 	private PokemonController pokemonController;
+	
+	@Autowired
+	private PokemonService pokemonService;
 
-	@Test
-    @DisplayName("Basic test for GET request")
+	/*@Test TODO: complete test cases
+    @DisplayName("Pokemon Controller GET request")
     void testGetRequest() {
-		ResponseEntity<?> responseEntity = pokemonController.healthcheck();
+		when(pokemonService.canDoubleDamage(new PokemonRelation(), new PokemonRelation())).thenReturn(true);
+		ResponseEntity<String> responseEntity = pokemonController.getRelation(12, 7, null);
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-	}
+	}*/
 }
